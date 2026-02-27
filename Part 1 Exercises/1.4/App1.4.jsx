edd4222d166f804c66b385c274160ee4370162d6
Aidan Sivak <sivakaidan@gmail.com>
2026-02-27 15:13:11 -0500

    Worked on 1.3-1.5

---
+++ Part 1 Exercises/1.4/App1.4.jsx
@@ -0,0 +1,69 @@
+//1.4 Arrays
+
+const Header = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+  <div>
+    <p> {props.part1} {props.exercises1} </p>
+    <p> {props.part2} {props.exercises2} </p>
+    <p> {props.part3} {props.exercises3} </p>
+  </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+  <div>
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  </div>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+  {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: 'Using preops to pass data',
+    exercises: 7
+  },
+  {
+    name: 'State of a component',
+    exercises: 14
+  }
+]
+
+  return (
+    //<div>
+      //<p> {parts[0].name} {parts[0].exercises} </p>
+      //<p> {parts[1].name} {parts[1].exercises} </p>
+      //<p> {parts[2].name} {parts[2].exercises} </p>
+    //</div>
+
+    <div>
+      <Header course={course} />
+      <Content part1={parts[0].name} exercises1={parts[0].exercises} 
+               part2={parts[1].name} exercises2={parts[1].exercises} 
+               part3={parts[2].name} exercises3={parts[2].exercises} />
+      <Total exercises1={parts[0].exercises} 
+             exercises2={parts[1].exercises} 
+             exercises3={parts[2].exercises}/>
+    </div>
+  )
+}
+
+export default App
--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -2,4 +2,6 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
+let counter = 1
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App counter={counter} />)
